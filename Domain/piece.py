e63c39bc624a94887e9ec507453b1ca1358d8c2e
seNNNz0rel <67145369+seNNNz0rel@users.noreply.github.com>
2022-03-08 23:24:48 +0200

    Add files via upload
    -now using a class Piece for each type of piece which made it easier to interact with pieces and modify their state
    -got rid of unnecessary modules

--- Domain/piece.py
+++ Domain/piece.py
@@ -1,17 +1,21 @@
 import pygame
-from Utils import draw
 
 
 class Pieces(pygame.sprite.Sprite):
-    def __init__(self, group, dim, type_of_piece):
-        self.groups = group
-        pygame.sprite.Sprite.__init__(self, self.groups)
+    def __init__(self, group, pos, type_of_piece):
+        super().__init__()
         self.type_of_piece = type_of_piece
-        self.__dim = dim
+        self.__pos = pos
         self.image = pygame.image.load("C:/Users/seNNNz0rel/Documents/ChessProject/Chess_Pieces/" +
                                        type_of_piece).convert_alpha()
-        self.rect = pygame.Rect(dim)
+        self.rect = pygame.Rect(pos)
 
+    def move_piece(self, new_pos):
+        self.__pos = new_pos
+        self.rect = pygame.Rect(new_pos)
+
+    def collides(self, pos):
+        return self.__pos[0] == pos[0] + 10 and self.__pos[1] == pos[1] + 10
 
 
 

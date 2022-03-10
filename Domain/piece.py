dfafc9a107fe6d8b43ea3a3b5f99bc13aecc3827
seNNNz0rel <67145369+seNNNz0rel@users.noreply.github.com>
2022-03-10 23:01:26 +0200

    Add files via upload
    -game is about 90% done
    -added turns for each player, now showing possible movements correctly
    -almost finished dealing with check and mate, a few cases left to solve
    -the program can detect if king is out of moves and a player has lost

--- Domain/piece.py
+++ Domain/piece.py
@@ -1,7 +1,19 @@
 import pygame
 
 
-class Pieces(pygame.sprite.Sprite):
+def find_piece_by_pos(pos, pieces):
+    """
+    Function to find a piece by a given position
+    """
+    for p in pieces:
+        if p.collides(pos):
+            return p
+
+
+class Piece(pygame.sprite.Sprite):
+    """
+    Class for Chess pieces
+    """
     def __init__(self, group, pos, type_of_piece):
         super().__init__()
         self.type_of_piece = type_of_piece
@@ -10,13 +22,23 @@ class Pieces(pygame.sprite.Sprite):
                                        type_of_piece).convert_alpha()
         self.rect = pygame.Rect(pos)
 
+    """
+    Getter for coordinates
+    """
+    def get_pos(self):
+        return self.__pos[0], self.__pos[1]
+
+    """
+    Function to move piece to a new position
+    """
     def move_piece(self, new_pos):
         self.__pos = new_pos
         self.rect = pygame.Rect(new_pos)
 
+    """
+    Function to check if a piece collides with given position
+    """
     def collides(self, pos):
+        if type(pos) == bool:
+            return False
         return self.__pos[0] == pos[0] + 10 and self.__pos[1] == pos[1] + 10
-
-
-
-
